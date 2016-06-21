f6d719f4ca0f1b9992f8db03b16c10e30572c9dd
Victor Mireyev <victor@opennodecloud.com>
2016-06-21 23:23:54 +0300

    Allow staff user and owner to delete user with OpenID connection (NC-1460)

---
+++ src/nodeconductor_auth_openid/perms.py
@@ -0,0 +1,7 @@
+from nodeconductor.core.permissions import FilteredCollaboratorsPermissionLogic
+
+
+PERMISSION_LOGICS = (
+    ('django_openid_auth.UserOpenID', FilteredCollaboratorsPermissionLogic(
+         user_field='user', any_permission=True)),
+)
