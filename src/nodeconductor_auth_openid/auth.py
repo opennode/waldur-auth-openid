36de722cba61eec3b96efba717abecfbe449e14d
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2016-10-26 15:45:30 +0300

    remove redundant imports
    - WAL-83

--- src/nodeconductor_auth_openid/auth.py
+++ src/nodeconductor_auth_openid/auth.py
@@ -1,12 +1,7 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django_openid_auth.auth import OpenIDBackend
-from django_openid_auth.exceptions import RequiredAttributeNotReturned
-
-
-User = get_user_model()
 
 
 class NodeConductorOpenIDBackend(OpenIDBackend):
