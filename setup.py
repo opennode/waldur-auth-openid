58a6d7b0c5dd1beaa23de5342230de9ea93c71c2
Jenkins <jenkins@opennodecloud.com>
2017-01-25 19:01:46 +0000

    Preparing new release: 0.2.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
 
 setup(
     name='nodeconductor-auth-openid',
-    version='0.2.2',
+    version='0.2.3',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://nodeconductor.com',
