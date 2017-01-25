e9d9d8e2e32c1f22ec807243b685929a3ff74a22
Victor Mireyev <victor@opennodecloud.com>
2017-01-25 20:02:51 +0300

    Redirect user to the frontend URL specified in configuration settings if OpenID login has failed (WAL-412)

--- src/nodeconductor_auth_openid/views.py
+++ src/nodeconductor_auth_openid/views.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import six
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
@@ -16,4 +17,11 @@ def login_complete(request):
     token = RefreshTokenMixin().refresh_token(request.user)
     url_template = settings.NODECONDUCTOR_AUTH_OPENID['LOGIN_URL_TEMPLATE']
     url = url_template.format(token=token.key)
+    return HttpResponseRedirect(url)
+
+
+def login_failed(request, message):
+    url_template = settings.NODECONDUCTOR_AUTH_OPENID['LOGIN_FAILED_URL_TEMPLATE']
+    params = six.moves.urllib.parse.urlencode(dict(message=message))
+    url = '%s?%s' % (url_template, params)
     return HttpResponseRedirect(url)
