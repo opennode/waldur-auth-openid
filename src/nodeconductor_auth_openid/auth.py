b8df66e24a185991be22cbd55e9ab3ba8a8acc3e
Victor Mireyev <victor@opennodecloud.com>
2017-02-07 14:51:38 +0300

    Move civil number parsing logic to authentication backend (WAL-473) Parse civil number from openid_response.

--- src/nodeconductor_auth_openid/auth.py
+++ src/nodeconductor_auth_openid/auth.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 
+import logging
+
 from django.conf import settings
 from django_openid_auth.auth import OpenIDBackend
 
-from . import utils
+
+logger = logging.getLogger(__name__)
 
 
 class NodeConductorOpenIDBackend(OpenIDBackend):
@@ -24,11 +27,10 @@ class NodeConductorOpenIDBackend(OpenIDBackend):
 
         # Civil number should be updated after each login because it can be changed or
         # defined for user.
-        if 'openid.identity' in details:
-            civil_number = utils.get_civil_number(details['openid.identity'])
-            if user.civil_number != civil_number:
-                user.civil_number = civil_number
-                updated_fields.append('civil_number')
+        civil_number = self._get_civil_number(openid_response)
+        if user.civil_number != civil_number:
+            user.civil_number = civil_number
+            updated_fields.append('civil_number')
 
         if updated_fields:
             user.save(update_fields=updated_fields)
@@ -36,12 +38,8 @@ class NodeConductorOpenIDBackend(OpenIDBackend):
     def create_user_from_openid(self, openid_response):
         user = super(NodeConductorOpenIDBackend, self).create_user_from_openid(openid_response)
 
-        openid_identity = openid_response.getSigned('http://specs.openid.net/auth/2.0', 'identity')
-        if openid_identity:
-            user.civil_number = utils.get_civil_number(openid_identity)
-
-        method_name = settings.NODECONDUCTOR_AUTH_OPENID.get('NAME', 'openid')
-        user.registration_method = method_name
+        user.civil_number = self._get_civil_number(openid_response)
+        user.registration_method = settings.NODECONDUCTOR_AUTH_OPENID.get('NAME', 'openid')
 
         user.save(update_fields=['civil_number', 'registration_method'])
         return user
@@ -49,3 +47,22 @@ class NodeConductorOpenIDBackend(OpenIDBackend):
     def _get_preferred_username(self, nickname, email):
         nickname = super(NodeConductorOpenIDBackend, self)._get_preferred_username(nickname, email)
         return nickname.replace(' ', '')
+
+    def _get_civil_number(self, openid_response):
+        """
+        Extract civil number from OpenID response.
+        Return empty string if personal code is not defined.
+
+        Expected openid.identity value: https://openid.ee/i/EE:<personal_code>
+        Example: https://openid.ee/i/EE:37605030299
+        Only the last part (<personal_code>) is stored as civil number.
+        """
+        openid_identity = openid_response.getSigned('http://specs.openid.net/auth/2.0', 'identity')
+
+        personal_code = openid_identity.split('/')[-1].split(':')[-1]
+        if personal_code.isdigit():
+            return personal_code
+        else:
+            logger.warning(
+                'Unable to parse openid.identity {}: personal code is not a numeric value'.format(openid_identity))
+            return ''
