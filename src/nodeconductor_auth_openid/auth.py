8d52416f27c1265e022fde65d7713a2d5aeebbbd
Pavel Marchuk <marchukpavelp@gmail.com>
2017-03-23 09:37:16 +0200

    Remove email-based authentication
     - wal-632

--- src/nodeconductor_auth_openid/auth.py
+++ src/nodeconductor_auth_openid/auth.py
@@ -57,11 +57,7 @@ class NodeConductorOpenIDBackend(OpenIDBackend):
         Example: https://openid.ee/i/EE:37605030299
         Only the last part (<personal_code>) is stored as civil number.
         """
-        if hasattr(openid_response, 'origin_identity'):
-            # XXX: Restore origin value that was patched by views.patched_parse_openid_response
-            openid_identity = openid_response.origin_identity
-        else:
-            openid_identity = openid_response.getSigned('http://specs.openid.net/auth/2.0', 'identity')
+        openid_identity = openid_response.getSigned('http://specs.openid.net/auth/2.0', 'identity')
 
         personal_code = openid_identity.split('/')[-1].split(':')[-1]
         if personal_code.isdigit():
