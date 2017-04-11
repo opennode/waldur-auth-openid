711842290a050b597d034964de758f4c16bda56e
Victor Mireyev <victor@opennodecloud.com>
2017-04-11 21:59:12 +0300

    Drop django-permission [WAL-710]

--- src/nodeconductor_auth_openid/perms.py
+++
@@ -1,6 +0,0 @@
-from nodeconductor.core.permissions import FilteredCollaboratorsPermissionLogic
-
-
-PERMISSION_LOGICS = (
-    ('django_openid_auth.UserOpenID', FilteredCollaboratorsPermissionLogic(user_field='user', any_permission=True)),
-)
