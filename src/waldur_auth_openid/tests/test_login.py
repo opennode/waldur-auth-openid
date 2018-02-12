2862755498e90199791e7393a6b60c885884f206
Victor Mireyev <victor@opennodecloud.com>
2018-02-12 20:13:30 +0300

    Move authentication callback URL templates and callback views from SAML2 plugin to core [WAL-1423]

--- src/waldur_auth_openid/tests/test_login.py
+++ src/waldur_auth_openid/tests/test_login.py
@@ -1,18 +1,16 @@
 from django.test import TestCase
 from rest_framework import status
 
+from waldur_core.core.tests.helpers import override_waldur_core_settings
 from ..views import login_failed
 
 
 class LoginTest(TestCase):
     def test_when_login_failed_user_is_redirected_to_frontend_with_message_as_query_param(self):
-        settings = {
-            'LOGIN_FAILED_URL_TEMPLATE': 'http://example.com/#/login_failed/'
-        }
         error_message = 'Disabled account'
         expected_url = 'http://example.com/#/login_failed/?message=Disabled+account'
 
-        with self.settings(WALDUR_AUTH_OPENID=settings):
+        with override_waldur_core_settings(LOGIN_FAILED_URL='http://example.com/#/login_failed/'):
             response = login_failed(None, error_message)
             self.assertEqual(response.status_code, status.HTTP_302_FOUND)
             self.assertEqual(response['Location'], expected_url)
